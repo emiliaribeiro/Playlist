25766cc48515de4dc9b47efa92f287941fa05bcd
EdX0t <edgardg@sapo.pt>
2015-05-20 16:11:51 +0100

    bean user

--- Playlist/src/main/java/ejb/UtilizadorBean.java
+++ Playlist/src/main/java/ejb/UtilizadorBean.java
@@ -1,10 +1,26 @@
 package ejb;
 
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import entities.Utilizador;
 
 @Named
 @RequestScoped
 public class UtilizadorBean {
 
+	private List<Utilizador> listaUsers;
+	@Inject
+	UtilizadorDAO userDao;
+
+	public List<Utilizador> getListaUsers() {
+		listaUsers = userDao.getListaUtilizadores();
+		return listaUsers;
+	}
+	
+	
+	
 }
